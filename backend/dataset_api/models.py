4c572666e995ff6797fa69bfa40852d01823b443
Ivan <ivanarizahuertas@gmail.com>
2023-07-14 13:40:38 +0200

    api dataset terminado, tablas creadas

--- backend/dataset_api/models.py
+++ backend/dataset_api/models.py
@@ -2,15 +2,48 @@ from django.conf import settings
 from django.db import models
 from user_api.models import models
 
+from datetime import datetime, timedelta
+
 # Create your models here.
-class Placeholder(models.Model):
+class Profesor(models.Model):
+    usuario = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    nombre = models.CharField(max_length=60)
+    apellido = models.CharField(max_length=60)
+
+class Asignatura(models.Model):
+    usuario = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    nombre = models.CharField(max_length=60)
+
+class Horario(models.Model):
+    usuario = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    asignatura = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
+    dia = models.IntegerField()
+    hora = models.IntegerField()
+
+class EstadoAsignatura(models.Model):
+    hoy = datetime.today()
+    hoy = hoy.strftime('%d/%m/%Y')
+
+    hora = datetime.today()
+    hora = hora.strftime('%H')
+
     usuario = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
     contentos = models.IntegerField(default=0)
     desanimados = models.IntegerField(default=0)
-    fecha = models.DateTimeField(auto_now_add=True)
-    hora = models.TimeField(auto_now_add=True)
-    asignatura = models.CharField(max_length=60)
-    profesor = models.CharField(max_length=60)
+    fecha = models.CharField(max_length=60,default=hoy)
+    hora = models.IntegerField(default=hora)
+    asignatura = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
--- backend/dataset_api/serializers.py
+++ backend/dataset_api/serializers.py
@@ -1,7 +1,22 @@
-from .models import Placeholder
+from .models import EstadoAsignatura, Profesor, Asignatura, Horario
 from rest_framework import serializers
 
-class PlaceholderSerializer(serializers.ModelSerializer):
+class EstadoAsignaturaSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Placeholder
-        fields = ['usuario_id','fecha','contentos','desanimados','asignatura','profesor']
+        model = EstadoAsignatura
+        fields = ['usuario_id','asignatura_id','profesor_id','fecha','hora','contentos','desanimados',]
+
+class ProfesorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profesor
+        fields = ['id','usuario_id','nombre','apellido']
+
+class AsignaturaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Asignatura
+        fields = ['id', 'usuario_id', 'nombre']
+
+class HorarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Horario
+        fields = ['id', 'dia', 'hora', 'asignatura_id', 'profesor_id', 'usuario_id']
