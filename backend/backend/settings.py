8d51607b20f683cd32dbe4fee420967368f826d7
IvanAriza <60660793+IvanAriza@users.noreply.github.com>
2023-07-16 11:47:16 +0200

    cambios cookies y pasar user_id

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,6 +34,11 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
+
 SESSION_COOKIE_SECURE = False
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
@@ -53,6 +58,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,7 +66,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
