8f3e7e276d1f3c05b090b3803ad8247d87389808
IvanAriza <ivanariza55@gmail.com>
2023-07-24 13:31:58 +0200

    cambio grafica

--- src/components/Graficolineasemana.jsx
+++ src/components/Graficolineasemana.jsx
@@ -39,28 +39,46 @@ useEffect(() => {
     const response = await client.get(`/api/showByAsignatura`, { params: {usuario_id:1, asignatura_id:asignatura_Id}});
     const data = response.data;
 
-    const contentos = data.map((item) => item.contentos);
-    const desanimados = data.map((item) => item.desanimados);
+    const contentos = data.map((item) => item.felicidad);
+    const desanimados = data.map((item) => item.tristeza);
+    const iracundos = data.map((item) => item.ira);
+    const odiosos = data.map((item) => item.odio);
+    const sorprendidos = data.map((item) => item.sorpresa);
 
     var totalContentos = 0;
     var totalDesanimados = 0;
+    var totalIra = 0;
+    var totalOdio = 0;
+    var totalSorprendidos = 0;
 
-    contentos.forEach(function(contento){
-      totalContentos += contento
+    contentos.forEach(function (contento) {
+        totalContentos += contento
     })
 
-    desanimados.forEach(function(desanimado){
-      totalDesanimados += desanimado
+    desanimados.forEach(function (desanimado) {
+        totalDesanimados += desanimado
+    })
+
+    iracundos.forEach(function (ira) {
+        totalIra += ira
+    })
+
+    odiosos.forEach(function (odio) {
+        totalOdio += odio
+    })
+
+    sorprendidos.forEach(function (sorprendido) {
+        totalSorprendidos += sorprendido
     })
 
     const chartData = {
-      labels: ["Contentos", "Desanimados"],
-      datasets: [
-        {
-          data: [totalContentos, totalDesanimados],
-          backgroundColor: ["green", "red"],
-        },
-      ],
+        labels: ["Contentos", "Desanimados", "Ira", "Odio", "Sorprendidos"],
+        datasets: [
+            {
+                data: [totalContentos, totalDesanimados, totalIra, totalOdio, totalSorprendidos],
+                backgroundColor: ["green", "blue", "red", "black", "yellow"],
+            },
+        ],
     };
 
     return chartData;
